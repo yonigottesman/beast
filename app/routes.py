bf5e5a66bb376b6d73371490a380045f84d407b6
Yonatan Gottesman <yonigo10@gmail.com>
2019-04-18 16:39:32 +0300

    Predict in flask

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import UploadForm
+from fastai.vision import load_learner, open_image, BytesIO
+import os
+
+
+model_path = os.getcwd() + '/ai/model'
+model_name = 'model.pkl'
+model = load_learner(model_path, model_name)
+
+
+def predict(image_bytes):
+    image = open_image(BytesIO(image_bytes))
+    x, class_, losses = model.predict(image)
+    print(x)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,7 +22,7 @@ def index():
     form = UploadForm()
     if form.validate_on_submit():
         image_bytes = form.image.data.read()
+        predict(image_bytes)
         flash('YONIGO')
         return redirect('/index')
     return render_template('index.html', title='upload', form=form)
-
