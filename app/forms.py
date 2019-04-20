29b2bf11d7dfd23b618da4d906b16794bd51a499
Yonatan Gottesman <yonigo10@gmail.com>
2019-04-20 22:18:00 +0300

    remove required field. again...

--- app/forms.py
+++ app/forms.py
@@ -8,6 +8,5 @@ from app import images
 
 
 class UploadForm(FlaskForm):
-    input_file = FileField('', id='inp_file', validators=[FileRequired()])
     image = HiddenField(id='inp_img')
     submit = SubmitField('Upload')
